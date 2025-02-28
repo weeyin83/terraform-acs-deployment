bb47b90096766bddf0d888a444dcfc34892818c0
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2025-02-28 15:57:26 +0000

    Create providers.tf

---
+++ acs-deployment/providers.tf
@@ -0,0 +1,30 @@
+##
+# Terraform Configuration
+##
+
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71, < 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1, < 4.0.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 2.2.0, < 3.0.0"
+    }
+  }
+}
+
+provider "azapi" {
+  # Configuration options
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "xxx-xxx-xxx-xxx-xxx"
+}
