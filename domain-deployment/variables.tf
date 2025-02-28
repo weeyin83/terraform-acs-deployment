0206024934bcd2942d5f5ef68a87a34fa73e69ab
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2025-02-28 15:55:22 +0000

    Create variables.tf

---
+++ domain-deployment/variables.tf
@@ -0,0 +1,13 @@
+
+##
+# Domain Variables
+##
+
+variable "custom_domain" {
+  description = "The custom domain for the email communication service"
+  type        = string
+  default     = "domainname.com"
+}
+
+
+
